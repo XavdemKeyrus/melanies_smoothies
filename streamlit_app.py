6b22b5dd84fe36e6e8a928c8104863d97a5e856e
XavdemKeyrus <xavier.demaignas@keyrus.com>
2024-05-31 15:54:42 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 # Import python packages
+# from snowflake.snowpark.functions import col
 import streamlit as st
-from snowflake.snowpark.functions import col
+from pyspark.sql.functions import col
 
 # Write directly to the app
 st.title("Customize Your Smoothie :cup_with_straw:")
